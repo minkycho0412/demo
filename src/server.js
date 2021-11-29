86beb483e327a199199dae3cd4894db21fee4083
minkycho0412 <minkycho0412@gmail.com>
2021-11-29 18:42:08 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-const handleListening = () => console.log(`Server listening on http://localhost:${PORT}🚀`);
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () => console.log(`Server listening on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
