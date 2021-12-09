3b24c1e016324c7e0fb2bca9667f2528083d0f29
minkycho0412 <minkycho0412@gmail.com>
2021-12-09 22:10:24 +0900

    css update2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,24 +7,30 @@ const promotionBtn = document.querySelector(".promotion");
 
 placeBtn.addEventListener("click", () => {
     placeBtn.style.backgroundColor = "#892E8D";
+    placeBtn.style.color ="white";
 });
 
 productBtn.addEventListener("click", () => {
     productBtn.style.backgroundColor = "#FF8C12";
+    productBtn.style.color ="white";
 });
 
 priceBtn.addEventListener("click", () => {
     priceBtn.style.backgroundColor = "#FF604E";
+    priceBtn.style.color ="white";
 });
 
 packagingBtn.addEventListener("click", () => {
     packagingBtn.style.backgroundColor = "#FF79A5";
+    packagingBtn.style.color ="white";
 });
 
 propositionBtn.addEventListener("click", () => {
     propositionBtn.style.backgroundColor = "#37BF3D";
+    propositionBtn.style.color ="white";
 });
 
 promotionBtn.addEventListener("click", () => {
     promotionBtn.style.backgroundColor = "#1898E3";
+    promotionBtn.style.color ="white";
 });
